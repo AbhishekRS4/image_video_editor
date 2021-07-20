ba9c1f2c72854cf62592cc6fb2e6b5bbdd2dd761
AbhishekRS4 <abhishek.r.satyanarayana.4@gmail.com>
2021-07-20 23:37:40 +0530

    refactor

--- src/app.py
+++ src/app.py
@@ -2,9 +2,10 @@ import os
 import sys
 import cv2
 import streamlit as st
-from video_utils_opencv import ImageWriterParams, VideoWriterParams, extract_images_from_video, create_video_from_images
 
-def imgs_to_vid_opencv():
+from video_utils_opencv import VideoToImageWriter, ImageToVideoWriter
+
+def images_to_video_opencv():
     st.title("Video creator from images - opencv")
     st.write(f"Current working directory - {os.getcwd()}")
     fps = st.sidebar.selectbox("FPS", [10, 15, 30, 60], index=0)
@@ -14,7 +15,7 @@ def imgs_to_vid_opencv():
     dir_images = st.sidebar.text_input("Directory to read images", "images")
     img_format = st.sidebar.selectbox("Image file format to be used", [".png", ".jpg"], index=0)
     video_encoder = st.sidebar.selectbox("Video encoder to use", ["mp4v", "xvid"], index=0)
-    video_writer_params = VideoWriterParams(
+    video_writer = ImageToVideoWriter(
         fps=fps,
         width=width,
         height=height,
@@ -25,10 +26,10 @@ def imgs_to_vid_opencv():
     )
     start_button = st.sidebar.button("Start video encoding")
     if start_button:
-        st.write(video_writer_params)
-        create_video_from_images(video_writer_params)
+        st.write(video_writer.params)
+        video_writer.generate_video_from_images()
 
-def vid_to_imgs_opencv():
+def video_to_images_opencv():
     st.title("Image extractor from video - opencv")
     st.write(f"Current working directory - {os.getcwd()}")
     file_video = st.sidebar.text_input("Video file to load", "sample.mp4")
@@ -36,7 +37,7 @@ def vid_to_imgs_opencv():
     img_prefix = st.sidebar.text_input("Prefix to use for image files", "image-")
     img_format = st.sidebar.selectbox("Image file format for saving", [".png", ".jpg"], index=0)
     img_id_start = st.sidebar.selectbox("Image start id to use", [10000, 100000, 1000000], index=0)
-    image_writer_params = ImageWriterParams(
+    image_writer = VideoToImageWriter(
         file_video=file_video,
         dir_images=dir_images,
         img_prefix=img_prefix,
@@ -45,8 +46,8 @@ def vid_to_imgs_opencv():
     )
     start_button = st.sidebar.button("Start image extraction")
     if start_button:
-        st.write(image_writer_params)
-        extract_images_from_video(image_writer_params)
+        st.write(image_writer.params)
+        image_writer.generate_images_from_video()
 
 def image_viewer():
     st.title("Image viewer")
@@ -83,8 +84,8 @@ def video_player():
 
 
 video_editor_modes = {
-    "Images to video opencv" : imgs_to_vid_opencv,
-    "Video to images opencv" : vid_to_imgs_opencv,
+    "Images to video opencv" : images_to_video_opencv,
+    "Video to images opencv" : video_to_images_opencv,
     "Image viewer" : image_viewer,
     "Video player" : video_player,
 }
