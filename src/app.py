5bfaf49814bfe92f910ac26e67be2d204e96f0df
AbhishekRS4 <abhishek.r.satyanarayana.4@gmail.com>
2021-07-21 19:28:01 +0530

    added option for video encoding using ffmpeg

--- src/app.py
+++ src/app.py
@@ -3,11 +3,65 @@ import sys
 import cv2
 import streamlit as st
 
+from video_utils_ffmpeg import FFMPEGImageToVideoWriter
 from video_utils_opencv import VideoToImageWriter, ImageToVideoWriter
+from file_utils import get_list_images, get_abs_path, delete_file, create_directory
+
+def images_to_video_ffmpeg():
+    st.title("Video creator from images - ffmpeg")
+    st.write(f"Current working dir - {os.getcwd()}")
+    fps = st.sidebar.selectbox("FPS", [10, 15, 30, 60], index=0)
+    width = st.sidebar.number_input("Image width", value=640)
+    height = st.sidebar.number_input("Image height", value=480)
+    file_video = st.sidebar.text_input("Video file to be created", "sample.mp4")
+    dir_images = st.sidebar.text_input("dir to read images", "images")
+    img_format = st.sidebar.selectbox("Image file format to be used", [".png", ".jpg"], index=0)
+    video_encoder = st.sidebar.selectbox("Video encoder to use", ["libx264", "libx265"], index=0)
+    start_button = st.sidebar.button("Start video encoding")
+
+    file_video = get_abs_path(file_video)
+    dir_images = get_abs_path(dir_images)
+
+    if start_button:
+        num_images = 0
+        ffmpeg_video_writer = FFMPEGImageToVideoWriter(fps=fps, width=width,
+            height=height, file_video=file_video, video_encoder=video_encoder)
+        st.write(ffmpeg_video_writer.ffmpeg_params)
+
+        if os.path.isdir(dir_images):
+            list_images = get_list_images(dir_images, img_format)
+            num_images = len(list_images)
+        else:
+            st.error(f"dir : {dir_images} not found")
+            return
+
+        if os.path.isfile(file_video):
+            delete_file(file_video)
+            st.write(f"Deleting existing video : {file_video}")
+
+        if num_images > fps:
+            st.write(f"Images dir : {dir_images}")
+            st.write(f"Starting video generation with {num_images} images")
+
+            img = cv2.imread(os.path.join(dir_images, list_images[0]))
+            if (img.shape[0] != height) or (img.shape[1] != width):
+                st.error(f"Image dimensions {img.shape[1]}x{img.shape[0]} mismatch with video encoding dimensions {width}x{height}")
+                return
+
+            ffmpeg_video_writer.open_ffmpeg_process()
+            progress_bar = st.progress(0.0)
+            for i in range(num_images):
+                img = cv2.imread(os.path.join(dir_images, list_images[i]))
+                ffmpeg_video_writer.write_image_to_video(img)
+                progress_bar.progress((i+1)/num_images)
+            ffmpeg_video_writer.close_ffmpeg_process()
+            st.success(f"Video successfully created, saved in {file_video}")
+        else:
+            st.error(f"Num images : {num_images}, not enough")
 
 def images_to_video_opencv():
     st.title("Video creator from images - opencv")
-    st.write(f"Current working directory - {os.getcwd()}")
+    st.write(f"Current working dir - {os.getcwd()}")
     fps = st.sidebar.selectbox("FPS", [10, 15, 30, 60], index=0)
     width = st.sidebar.number_input("Image width", value=640)
     height = st.sidebar.number_input("Image height", value=480)
@@ -31,7 +85,7 @@ def images_to_video_opencv():
 
 def video_to_images_opencv():
     st.title("Image extractor from video - opencv")
-    st.write(f"Current working directory - {os.getcwd()}")
+    st.write(f"Current working dir - {os.getcwd()}")
     file_video = st.sidebar.text_input("Video file to load", "sample.mp4")
     dir_images = st.sidebar.text_input("Directory to save images", "images")
     img_prefix = st.sidebar.text_input("Prefix to use for image files", "image-")
@@ -51,11 +105,11 @@ def video_to_images_opencv():
 
 def image_viewer():
     st.title("Image viewer")
-    st.write(f"Current working directory - {os.getcwd()}")
+    st.write(f"Current working dir - {os.getcwd()}")
     dir_images = st.sidebar.text_input("Directory to load images", "images")
     img_format = st.sidebar.selectbox("Image file format for saving", [".png", ".jpg"], index=0)
     try:
-        st.header(f"Image directory - {dir_images}")
+        st.header(f"Image dir - {dir_images}")
         list_images = sorted([f for f in os.listdir(dir_images) if f.endswith(img_format)])
         num_images = len(list_images)
         image_id = st.sidebar.slider(
@@ -66,12 +120,12 @@ def image_viewer():
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         st.image(img, caption=list_images[image_id])
     except:
-        st.error(f"Images directory {dir_images} does not contain images")
+        st.error(f"Images dir {dir_images} does not contain images")
         return
 
 def video_player():
     st.title("Video player")
-    st.write(f"Current working directory - {os.getcwd()}")
+    st.write(f"Current working dir - {os.getcwd()}")
     file_video = st.sidebar.text_input("Video file", "sample.mp4")
     try:
         st.header(f"Playing video file - {file_video}")
@@ -82,8 +136,8 @@ def video_player():
         st.error(f"Error in loading the video file - {file_video}")
         return
 
-
 video_editor_modes = {
+    "Images to video ffmpeg" : images_to_video_ffmpeg,
     "Images to video opencv" : images_to_video_opencv,
     "Video to images opencv" : video_to_images_opencv,
     "Image viewer" : image_viewer,
