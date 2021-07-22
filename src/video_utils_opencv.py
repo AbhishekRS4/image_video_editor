6d719435b0ae910ee7e7bdf8489f490e74443320
AbhishekRS4 <abhishek.r.satyanarayana.4@gmail.com>
2021-07-22 20:53:01 +0530

    simplified class to generate video using opencv video writer

--- src/video_utils_opencv.py
+++ src/video_utils_opencv.py
@@ -2,20 +2,7 @@ import os
 import sys
 import cv2
 import numpy as np
-import streamlit as st
 from dataclasses import dataclass
-
-def create_directory(directory_path):
-    """
-    Parameters
-    ----------
-    directory_path (str) : full path of directory
-    """
-    if not os.path.isdir(directory_path):
-        os.makedirs(directory_path)
-        st.write(f"Directory created: {directory_path}")
-    else:
-        st.write(f"Directory {directory_path} already exists")
 
 class VideoToImageWriter:
     def __init__(self, file_video, dir_images, img_prefix, img_format, img_id_start):
@@ -74,7 +61,7 @@ class VideoToImageWriter:
         st.success("Image extraction from the video completed")
 
 class ImageToVideoWriter:
-    def __init__(self, fps, width, height, file_video, dir_images, img_format, video_encoder):
+    def __init__(self, fps, width, height, file_video, video_encoder):
         """
         Parameters
         ----------
@@ -82,12 +69,11 @@ class ImageToVideoWriter:
         width (int) : width of the video
         height (int) : height of the video
         file_video (str) : full path of video file
-        dir_images (str) : full path of directory containing images
-        img_format (str) : image file format to be used
         video_encoder (str) : video encoder to be used
         """
         self.params = self.VideoParams(fps=fps, width=width, height=height, file_video=file_video,
-            dir_images=dir_images, img_format=img_format, video_encoder=video_encoder)
+            video_encoder=video_encoder)
+        self.video_writer = None
 
     @dataclass
     class VideoParams:
@@ -95,53 +81,18 @@ class ImageToVideoWriter:
         width : int
         height : int
         file_video : str
-        dir_images : str
-        img_format : str
         video_encoder : str
 
-    def generate_video_from_images(self):
-        if not os.path.isdir(self.params.dir_images):
-            st.error(f"Images directory {self.params.dir_images} does not exist")
-            return
+    def init_video_writer(self):
+        self.video_writer = cv2.VideoWriter(self.params.file_video,
+            cv2.VideoWriter_fourcc(*self.params.video_encoder), self.params.fps,
+            (self.params.width, self.params.height))
+        return
 
-        dir_out = os.path.dirname(self.params.file_video)
-        if not dir_out == "":
-            create_directory(dir_out)
-        else:
-            dir_out = os.getcwd()
+    def close_video_writer(self):
+        if self.video_writer is not None:
+            self.video_writer.release()
+        return
 
-        if os.path.isfile(self.params.file_video):
-            st.write(f"Deleteing already present {self.params.file_video}")
-            os.unlink(self.params.file_video)
-
-        list_images = sorted(
-            [f for f in os.listdir(self.params.dir_images)\
-            if f.endswith(self.params.img_format)]
-        )
-        num_images = len(list_images)
-        st.write(f"Number of images to be converted into a video : {num_images}")
-        st.write(f"Video creation params - fps : {self.params.fps}, video_resolution : {self.params.width}x{self.params.height}")
-        img = cv2.imread(os.path.join(self.params.dir_images, list_images[0]))
-        if (img.shape[0] != self.params.height or \
-            img.shape[1] != self.params.width):
-            st.error(f"Image dimensions {img.shape[1]}x{img.shape[0]} mismatch with video dimensions {self.params.width}x{self.params.height}")
-            return
-
-        video_writer = cv2.VideoWriter(self.params.file_video,
-            cv2.VideoWriter_fourcc(*self.params.video_encoder),
-            self.params.fps, (self.params.width, self.params.height)
-        )
-
-        st.write("Video generation started")
-        progress_bar = st.progress(0.0)
-        for i in range(num_images):
-            try:
-                img = cv2.imread(os.path.join(self.params.dir_images, list_images[i]))
-                video_writer.write(img)
-                progress_bar.progress((i + 1)/ num_images)
-            except IOError:
-                st.error("Unknown error in image or video file handling")
-                return
-        video_writer.release()
-        st.write(f"{self.params.file_video} has been successfully created in : {dir_out}")
-        st.success("Video encoding from images completed")
+    def write_image_to_video(self, image_array):
+        self.video_writer.write(image_array)
