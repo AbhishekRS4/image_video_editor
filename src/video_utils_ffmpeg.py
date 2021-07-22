738e7af9385b1a403db8d7005b3b8e4047d83c37
AbhishekRS4 <abhishek.r.satyanarayana.4@gmail.com>
2021-07-22 20:00:08 +0530

    added class for video generator with saved images using ffmpeg

--- src/video_utils_ffmpeg.py
+++ src/video_utils_ffmpeg.py
@@ -2,52 +2,42 @@ import os
 import sys
 import argparse
 import numpy as np
-import streamlit as st
 from dataclasses import dataclass
 from subprocess import Popen, PIPE, DEVNULL
 
-@dataclass
-class FFMPEGVideoWriterParams:
-    fps : int
-    crf : int
-    dir_images : str
-    file_video : str
-    img_format : str
-    video_encoder : str
-    video_pixel_format : str
+class FFMPEGSavedImageToVideoWriter:
+    def __init__(self, dir_images, file_video, fps=30, crf=23, img_format=".png", video_encoder="libx264", video_pixel_format="yuv420p"):
+        self.cmd_ffmpeg = None
+        self.ffmpeg_params = self.FFMPEGParams(fps=fps, crf=crf, dir_images=dir_images,
+            file_video=file_video, img_format=img_format, video_encoder=video_encoder,
+            video_pixel_format=video_pixel_format)
 
-def create_video_from_images(ffmpeg_params):
-    """
-    Parameters
-    ----------
-    ffmpeg_params (FFMPEGVideoWriterParams) : object of class FFMPEGVideoWriterParams
-    """
-    if not os.path.isdir(ffmpeg_params.dir_images):
-        st.error(f"Images directory {ffmpeg_params.dir_images} does not exist")
-        return
+    @dataclass
+    class FFMPEGParams:
+        fps : int
+        crf : int
+        dir_images : str
+        file_video : str
+        img_format : str
+        video_encoder : str
+        video_pixel_format : str
 
-    if os.path.isfile(ffmpeg_params.file_video):
-        st.write(f"Deleting the already existing video file : {ffmpeg_params.file_video}")
-        os.unlink(ffmpeg_params.file_video)
+    def generate_video_from_saved_images(self):
+        self.cmd_ffmpeg = f"ffmpeg -framerate {self.ffmpeg_params.fps} \
+            -pattern_type glob -i '{self.ffmpeg_params.dir_images}/*{self.ffmpeg_params.img_format}' \
+            -c:v {self.ffmpeg_params.video_encoder} -profile:v high -crf {self.ffmpeg_params.crf} \
+            -pix_fmt {self.ffmpeg_params.video_pixel_format} {self.ffmpeg_params.file_video}"
+        print(f"{self.cmd_ffmpeg}")
 
-    cmd_ffmpeg = f"ffmpeg -framerate {ffmpeg_params.fps} -pattern_type glob -i '{ffmpeg_params.dir_images}/*{ffmpeg_params.img_format}' -c:v {ffmpeg_params.video_encoder} -profile:v high -crf {ffmpeg_params.crf} -pix_fmt {ffmpeg_params.video_pixel_format} {ffmpeg_params.file_video}"
-    st.write(f"{cmd_ffmpeg}")
-
-    try:
-        os.system(cmd_ffmpeg)
-    except:
-        st.error("error while running ffmpeg")
+        os.system(self.cmd_ffmpeg)
         return
 
 class FFMPEGImageToVideoWriter:
     def __init__(self, file_video, fps=30, video_encoder="libx264", width=640, height=480, pixel_format_in="bgr24", pixel_format_out="yuv420p"):
         self.dtype = np.uint8
-        self.ffmpeg_params = self.FFMPEGParams(
-            fps=fps, width=width, height=height,
+        self.ffmpeg_params = self.FFMPEGParams(fps=fps, width=width, height=height,
             file_video=file_video, video_encoder=video_encoder,
-            pixel_format_in=pixel_format_in,
-            pixel_format_out=pixel_format_out
-        )
+            pixel_format_in=pixel_format_in, pixel_format_out=pixel_format_out)
         self.cmd_ffmpeg = self.get_ffmpeg_command()
         self.popen_params = {"stdout": DEVNULL, "stdin": PIPE}
         self.process = None
@@ -77,12 +67,10 @@ class FFMPEGImageToVideoWriter:
             "-s", f"{self.ffmpeg_params.width}x{self.ffmpeg_params.height}",
             "-pix_fmt", self.ffmpeg_params.pixel_format_in,
             "-r", f"{self.ffmpeg_params.fps}",
-            "-an",
-            "-i", "-",
+            "-an", "-i", "-",
             "-c:v", self.ffmpeg_params.video_encoder,
             "-pix_fmt", self.ffmpeg_params.pixel_format_out,
-            self.ffmpeg_params.file_video
-        ]
+            self.ffmpeg_params.file_video]
         return cmd_ffmpeg
 
     def write_image_to_video(self, image_array):
